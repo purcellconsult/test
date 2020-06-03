3d8709a534cb6ca3247a6522d2166259775a30a6
purcellconsult <purcellconsult@gmail.com>
2020-06-03 10:43:50 -0700

    Add files via upload
    Uploading the interactive.py file

---
+++ interactive.py
@@ -0,0 +1,51 @@
+############################
+# This program will read in
+# user input by using the
+# input() function in python.
+# We can store the user input
+# in a variable and manipulate
+# it.
+#
+##############################
+
+# the addition functionality of our calc
+
+x1 = float(input("Enter in a number add "))
+print("The number entered is ", x1)
+
+y1 = float(input("Enter in another number to add "))
+print("The second number entered is ", y1)
+
+print(x1, '+', y1, '=', x1 + y1)  # sum numbers x and y
+
+# the subtraction functionality of our calc
+
+x2 = float(input("Enter in a  number to subtract "))
+y2 = float(input("Enter in the second number "))
+print(x2, '-', y2, '=', x2 - y2)
+
+# the multiplication functionality of our calc
+
+x3 = float(input("Enter in a number to multiply "))
+y3 = float(input("Enter in another number to multiply "))
+print(x3, '*', y3, '=', x3 * y3)
+
+# division functionality for our calc
+
+x4 = float(input("Enter in a number to divide "))
+y4 = float(input("Enter in another number to divide "))
+print(x4, '/', y4, '=', x4 / y4)
+
+
+
+# implement the power functionality using **
+
+# implement the mod functionality %
+
+# import the math module, and implement ANY one functionality
+
+# Create a GitHub account
+
+
+
+
